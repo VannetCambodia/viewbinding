6acd636b9389e9ddfb28226e0cd856cee75c82d9
Veng Vannet <vannetvengpp@gmail.com>
2021-02-20 13:09:09 +0700

    View Binding in Android

--- app/src/main/java/com/vannet/viewbinding/MainActivity.kt
+++ app/src/main/java/com/vannet/viewbinding/MainActivity.kt
@@ -2,10 +2,21 @@ package com.vannet.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.vannet.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.view.text = "Veng Vannet"
+    }
+
+    fun btnHandler(view: View) {
+        Toast.makeText(applicationContext,"Hello",Toast.LENGTH_SHORT).show()
     }
 }
